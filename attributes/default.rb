ddbdfca04711ca8191293dc1189bc340b217b0d4
Jingzhao Ou <jingzhao@gowall.com>
2015-09-28 01:40:53 -0700

    reapply changes to default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -60,8 +60,12 @@ default['rabbitmq']['erlang_cookie'] = 'AnyAlphaNumericStringWillDo'
 default['rabbitmq']['cluster_partition_handling'] = 'ignore'
 
 default['rabbitmq']['clustering']['use_auto_clustering'] = false
-default['rabbitmq']['clustering']['cluster_name'] = nil
-default['rabbitmq']['clustering']['cluster_nodes'] = []
+default['rabbitmq']['clustering']['cluster_name'] = 'gowall'
+default['rabbitmq']['clustering']['cluster_nodes'] = [
+    { :name => 'rabbit@rabbit1', :type => 'disc' },
+    { :name => 'rabbit@rabbit2', :type => 'disc' }
+    { :name => 'rabbit@rabbit3', :type => 'disc' }
+]
 
 # Manual clustering
 # - Node type : master | slave
